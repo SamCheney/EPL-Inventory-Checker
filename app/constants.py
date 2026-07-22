6bed0b5ba9b68f7e487fb941389da4283e0a4e51
SamCheney <samjammer1989@gmail.com>
2026-07-22 14:09:32 -0500

    v1.0.1 added automatic update

--- app/constants.py
+++ app/constants.py
@@ -3,7 +3,7 @@ import re
 VIP_URL = "https://vip.hobartservice.com/"
 
 APP_NAME = "EPL INVENTORY CHECKER"
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.0.1"
 
 SEARCH_BOX = "#ctl00_SearchBoxPlaceHolder_ItemIDTextBox"
 SEARCH_BUTTON = "#ctl00_SearchBoxPlaceHolder_SearchButton"
